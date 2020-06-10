754bd51257a05c7c7a2ce741fde0369ae990c378
windmip <horvath0balint@gmail.com>
2020-06-10 19:24:28 +0200

    Turn off int-in-bool-context and misleading-indentation warnings in cmakelist. Fix unsigned int/int warning in tools

--- src/tools.cpp
+++ src/tools.cpp
@@ -14,6 +14,28 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+	// Must be similar size, must contain elements
+	if (!estimations.size() || estimations.size()!=ground_truth.size())
+		return rmse;
+
+	VectorXd sum(4);
+	sum << 0,0,0,0;
+
+	// TODO: optimize code eliminating temporary varibles 
+	for(unsigned int i=0; i < estimations.size(); ++i)
+	{
+		VectorXd sb = estimations[i] - ground_truth[i];
+		VectorXd tmp = sb.array() * sb.array();
+		sum = sum + tmp;
+	}	
+	rmse = sum/estimations.size();
+	rmse = rmse.array().sqrt();
+
+	return rmse;	
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -21,4 +43,23 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    * TODO:
    * Calculate a Jacobian here.
    */
+	MatrixXd Hj(3,4);
+	
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	if (px == 0 && py == 0)
+		return Hj;
+
+	double p_pow = pow(px,2) + pow(py,2);
+	double pv_mult_1 = py * (vx * py - vy * px);
+	double pv_mult_2 = px * (vy * px - vx * py);
+
+	Hj <<	px/sqrt(p_pow),						py/sqrt(p_pow),						0,							0,
+				-py/p_pow,								px/p_pow,									0,							0,	
+				pv_mult_1/pow(p_pow,1.5), pv_mult_2/pow(p_pow,1.5), px/sqrt(p_pow), py/sqrt(p_pow);
+
+	return Hj;
 }
