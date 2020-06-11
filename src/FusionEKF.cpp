8d9def1093019824cd72c036f2e5dfe50c3c499e
windmip <horvath0balint@gmail.com>
2020-06-11 15:05:03 +0200

    FIx first measurement

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -52,6 +52,10 @@ FusionEKF::FusionEKF() {
 				0, 0, 1, 0,
 				0, 0, 0, 1;
 
+
+	H_laser_ << 1,0,0,0,
+							0,1,0,0;
+
 	MatrixXd Q = MatrixXd(4,4);
 	ekf_.Init(x,P,F,H_laser_,R_laser_,Q);
 }
@@ -73,9 +77,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      */
 
     // first measurement
-    cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       // TODO: Convert radar from polar to cartesian coordinates 
@@ -84,7 +86,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 			double phi = measurement_pack.raw_measurements_[1];
 			double rho_dot = measurement_pack.raw_measurements_[2];
 
-			ekf_.x_ << rho * cos(phi) , rho * sin(phi) , rho_dot * cos(phi) , rho_dot * sin(phi); 
+			ekf_.x_ << rho * cos(phi) , rho * sin(phi) , 0 , 0; 
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       // TODO: Initialize state.
