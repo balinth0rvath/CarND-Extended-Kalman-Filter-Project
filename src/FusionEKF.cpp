d1ca0d0671281a4b1ba3ed235df4f7e5ce6bbc87
windmip <horvath0balint@gmail.com>
2020-06-11 12:04:17 +0200

    Initialize state when radar data comes first. Set H,R and z for radar and lidar updates

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -80,13 +80,17 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       // TODO: Convert radar from polar to cartesian coordinates 
       //         and initialize state.
+			double rho = measurement_pack.raw_measurements_[0];
+			double phi = measurement_pack.raw_measurements_[1];
+			double rho_dot = measurement_pack.raw_measurements_[2];
 
+			ekf_.x_ << rho * cos(phi) , rho * sin(phi) , rho_dot * cos(phi) , rho_dot * sin(phi); 
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       // TODO: Initialize state.
 			ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1],0,0;
-			previous_timestamp_ = measurement_pack.timestamp_;
     }
+		previous_timestamp_ = measurement_pack.timestamp_;
 
     // done initializing, no need to predict or update
     is_initialized_ = true;
@@ -133,10 +137,18 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
-
+		VectorXd z(3);
+		ekf_.H_ = tools.CalculateJacobian(ekf_.x_);	
+		ekf_.R_ = R_radar_;
+		z << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1],
+					measurement_pack.raw_measurements_[2];
+		ekf_.UpdateEKF(z);
+		 
   } else {
     // TODO: Laser updates
 		VectorXd z(2);
+		ekf_.H_ = H_laser_;
+		ekf_.R_ = R_laser_;
 		z << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1];
 		ekf_.Update(z);
   }
